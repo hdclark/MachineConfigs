71d2f13b6a8f2bbd7b777b0343eba6afcfcd5505
Haley Clark <gmail.com@hdeanclark>
2020-08-24 19:09:04 -0700

    Added script drive an interactive bidirectional merge between repo and system dotfiles.

---
+++ interactively_merge_dotfiles.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This script can be used to merge files in dotfiles/ with the equivalent dotfile in ~/ .
+find ./dotfiles/ -type f -print0 |
+  sed -z -e 's@\([^/]*\)$@\1 ~/\1@' |
+  xargs -0 -I '{}' -P 1 -n 1 -r bash -c "
+    echo '=== Merging files: {} ==='
+    cmp {} &>/dev/null && echo '...files are identical, nothing to do.' ||
+      meld {}"
+
