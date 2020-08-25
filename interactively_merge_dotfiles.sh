bfe2508df75f141e9684799b3241e4c45d1e7e83
Haley Clark <gmail.com@hdeanclark>
2020-08-25 12:15:38 -0700

    Permit multi-directory paths.

--- interactively_merge_dotfiles.sh
+++ interactively_merge_dotfiles.sh
@@ -2,7 +2,7 @@
 
 # This script can be used to merge files in dotfiles/ with the equivalent dotfile in ~/ .
 find ./dotfiles/ -type f -print0 |
-  sed -z -e 's@\([^/]*\)$@\1 ~/\1@' |
+sed -z -e 's@\(.*dotfiles/\)\(.*\)$@\1\2 ~/\2@' |
   xargs -0 -I '{}' -P 1 -n 1 -r bash -c "
     echo '=== Merging files: {} ==='
     cmp {} &>/dev/null && echo '...files are identical, nothing to do.' ||
