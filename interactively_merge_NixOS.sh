4c3ddcf3912de5b33d48fee5fc2998532132aefc
Haley Clark <gmail.com@hdeanclark>
2020-08-25 19:30:14 -0700

    Added MERGETOOL environment configuration.
    System files can be edited effectively by setting MERGETOOL='sudo vimdiff' or equivalent.

--- interactively_merge_NixOS.sh
+++ interactively_merge_NixOS.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+if [ -z "$MERGETOOL" ] ; then
+    export MERGETOOL=meld
+fi
+
 # This script can be used to merge files in NixOS/ with the equivalent files in / on a NixOS system.
 find ./NixOS/ -type f -print0 |
 sed -z -e 's@\(.*NixOS/\)\(.*\)$@\1\2 /\2@' |
   xargs -0 -I '{}' -P 1 -n 1 -r bash -c "
     echo '=== Merging files: {} ==='
     cmp {} &>/dev/null && echo '...files are identical, nothing to do.' ||
-      meld {}"
+      $MERGETOOL {}"
 
--- interactively_merge_dotfiles.sh
+++ interactively_merge_dotfiles.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+if [ -z "$MERGETOOL" ] ; then
+    export MERGETOOL=meld
+fi
+
 # This script can be used to merge files in dotfiles/ with the equivalent dotfile in ~/ .
 find ./dotfiles/ -type f -print0 |
 sed -z -e 's@\(.*dotfiles/\)\(.*\)$@\1\2 ~/\2@' |
   xargs -0 -I '{}' -P 1 -n 1 -r bash -c "
     echo '=== Merging files: {} ==='
     cmp {} &>/dev/null && echo '...files are identical, nothing to do.' ||
-      meld {}"
+      $MERGETOOL {}"
 
